07dfdaafe14fe6ad6ded10737ef6fe08b49e32dc
Spcie <m15188159686@163.com>
2019-06-02 19:56:47 +0800

    Update callback.cpp

--- Src/Callback/callback.cpp
+++ Src/Callback/callback.cpp
@@ -4,12 +4,95 @@
 
 namespace Infra
 {
+
 template<class T, typename P1, typename P2>
-class CCallBack
+class CCallback
 {
 public:
-	CCallBack(unsigned int maxAttach);
-	virtual ~CCallBack();
+	CCallback();
+	virtual ~CCallback();
+	typedef void (T::*cbfun)(P1, P2);
+	bool attach(T* pInst, cbfun fun);
+	bool detach(T* pInst, cbfun fun);
+	bool isAttach();
+	void operator()(P1 p1, P2 p2);
+private:
+	struct AttachInfo
+	{
+		T* instance;
+		cbfun fun;
+	};
+
+	struct AttachInfo m_attchInfo;
+};
+
+template<class T, typename P1, typename P2>
+CCallback<T, P1, P2>::CCallback()
+{
+	m_attchInfo.instance = NULL;
+	m_attchInfo.fun = NULL;
+}
+
+template<class T, typename P1, typename P2>
+CCallback<T, P1, P2>::~CCallback()
+{
+
+}
+
+template<class T, typename P1, typename P2>
+bool CCallback<T, P1, P2>::attach(T* pInst, cbfun fun)
+{
+	if (m_attchInfo.instance == NULL || m_attchInfo.fun == NULL)
+	{
+		return false;
+	}
+	
+	m_attchInfo.instance = pInst;
+	m_attchInfo.fun = fun;
+	return true;
+}
+
+template<class T, typename P1, typename P2>
+bool CCallback<T, P1, P2>::detach(T* pInst, cbfun fun)
+{
+	if (m_attchInfo.instance != pInst || m_attchInfo.fun != fun)
+	{
+		return false;
+	}
+
+	m_attchInfo.instance = NULL;
+	m_attchInfo.fun = NULL;
+}
+
+template<class T, typename P1, typename P2>
+bool CCallback<T, P1, P2>::isAttach()
+{
+
+	if (m_attchInfo.instance == NULL || m_attchInfo.fun == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+template<class T, typename P1, typename P2>
+void CCallback<T, P1, P2>::operator()(P1 p1, P2 p2)
+{
+	if (m_attchInfo.instance != NULL && m_attchInfo.fun != NULL)
+	{
+		((m_attchInfo.instance)->*(m_attchInfo.fun))(p1, p2);
+	}
+}
+
+template<class T, typename P1, typename P2>
+class CObserver
+{
+public:
+	CObserver(unsigned int maxAttach);
+	virtual ~CObserver();
 	typedef void (T::*cbfun)(P1, P2);
 	bool attach(T* pInst, cbfun fun);
 	bool detach(T* pInst, cbfun fun);
@@ -24,30 +107,35 @@ private:
 	};
 
 	unsigned int m_maxAttach;
-	Infra::Clink* m_link;
+	CLink m_link;
 };
 
 template<class T, typename P1, typename P2>
-CCallBack<T, P1, P2>::CCallBack(unsigned int maxAttach)
+CObserver<T, P1, P2>::CObserver(unsigned int maxAttach)
 :m_maxAttach(maxAttach)
-,m_link()
+,m_link(NULL)
 {
 
 }
 
 template<class T, typename P1, typename P2>
-CCallBack<T, P1, P2>::~CCallBack()
+CObserver<T, P1, P2>::~CObserver()
 {
 	struct AttachInfo * pInfo;
-	while(m_link.reduce((void*)&pInfo) > 0)
+	while(m_link.reduce((void**)&pInfo) > 0)
 	{
 		delete pInfo;
+		pInfo = NULL;
 	}
 }
 
 template<class T, typename P1, typename P2>
-bool CCallBack<T, P1, P2>::attach(T* pInst, cbfun fun)
+bool CObserver<T, P1, P2>::attach(T* pInst, cbfun fun)
 {
+	if (m_link.linkSize() >= m_maxAttach)
+	{
+		return false;
+	}
 	if (getAttachPos(pInst, fun) >= 0)
 	{
 		return false;
@@ -60,7 +148,7 @@ bool CCallBack<T, P1, P2>::attach(T* pInst, cbfun fun)
 }
 
 template<class T, typename P1, typename P2>
-bool CCallBack<T, P1, P2>::detach(T* pInst, cbfun fun)
+bool CObserver<T, P1, P2>::detach(T* pInst, cbfun fun)
 {
 	struct AttachInfo * pInfo;
 	int pos = getAttachPos(pInst, fun);
@@ -69,31 +157,46 @@ bool CCallBack<T, P1, P2>::detach(T* pInst, cbfun fun)
 		return false;
 	}
 
-	m_link.remove((void*)&pInfo, pos);
+	m_link.remove((void**)&pInfo, pos);
 	delete pInfo;
 	pInfo = NULL;
 	return true;
 }
 
 template<class T, typename P1, typename P2>
-void CCallBack<T, P1, P2>::operator()(P1 p1, P2 p2)
+bool CObserver<T, P1, P2>::isAttach(T* pInst, cbfun fun)
 {
+	if (getAttachPos(pInst, fun) < 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+	
+}
+
+template<class T, typename P1, typename P2>
+void CObserver<T, P1, P2>::operator()(P1 p1, P2 p2)
+{
+	struct AttachInfo * pInfo;
 	//先attach的先回调
-	for (unsigned int i = 0; i > m_link.linksize(); i++)
+	for (unsigned int i = 0; i > m_link.linkSize(); i++)
 	{
 		pInfo = (struct AttachInfo *)m_link.get(i);
 		if (pInfo != NULL && pInfo->instance != NULL && pInfo->fun != NULL)
 		{
-			(pInfo->instance)->(pInfo->fun)(p1, p2);
+			((pInfo->instance)->*(pInfo->fun))(p1, p2);
 		}
 	}
 }
 
 template<class T, typename P1, typename P2>
-int CCallBack<T, P1, P2>::getAttachPos(T* pInst, cbfun fun)
+int CObserver<T, P1, P2>::getAttachPos(T* pInst, cbfun fun)
 {
 	struct AttachInfo * pInfo;
-	for (unsigned int i = 0; i > m_link.linksize(); i++)
+	for (unsigned int i = 0; i > m_link.linkSize(); i++)
 	{
 		pInfo = (struct AttachInfo *)m_link.get(i);
 		if (pInfo != NULL && pInfo->instance == pInst && pInfo->fun == fun)

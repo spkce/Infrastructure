1e8a2db983931745d39c235824b27e3122bb234b
Spcie <m15188159686@163.com>
2019-06-02 18:02:11 +0800

    Update callback.cpp

--- Src/Callback/callback.cpp
+++ Src/Callback/callback.cpp
@@ -12,7 +12,10 @@ public:
 	typedef void (T::*cbfun)(P1, P2);
 	bool attach(T* pInst, cbfun fun);
 	bool detach(T* pInst, cbfun fun);
+	bool isAttach(T* pInst, cbfun fun);
+	void operator()(P1 p1, P2 p2);
 private:
+	int getAttachPos(T* pInst, cbfun fun);
 	struct AttachInfo
 	{
 		T* instance;
@@ -41,6 +44,10 @@ CCallBack::~CCallBack()
 
 bool CCallBack::attach(T* pInst, cbfun fun)
 {
+	if (getAttachPos(pInst, fun) >= 0)
+	{
+		return false;
+	}
 	struct AttachInfo * pInfo = new struct AttachInfo;
 	pInfo->instance = pInst;
 	pInfo->fun = fun;
@@ -51,19 +58,43 @@ bool CCallBack::attach(T* pInst, cbfun fun)
 bool CCallBack::detach(T* pInst, cbfun fun)
 {
 	struct AttachInfo * pInfo;
-	unsigned int i;
-	for (i = m_link.linksize(); i > 0; i--)
+	int pos = getAttachPos(pInst, fun);
+	if (pos < 0)
 	{
-		pInfo = (struct AttachInfo *)m_link.get();
-		if (pInfo->instance == pInst && pInfo->fun == fun)
-		{
-			m_link.remove((void*)&pInfo, i - 1);
-			return true;
-		}
+		return false;
 	}
 
-	return false;
+	m_link.remove((void*)&pInfo, pos);
+	delete pInfo;
+	pInfo = NULL;
+	return true;
 }
 
+void CCallBack::operator()(P1 p1, P2 p2)
+{
+	//先attach的先回调
+	for (unsigned int i = 0; i > m_link.linksize(); i++)
+	{
+		pInfo = (struct AttachInfo *)m_link.get(i);
+		if (pInfo != NULL && pInfo->instance != NULL && pInfo->fun != NULL)
+		{
+			(pInfo->instance)->(pInfo->fun)(p1, p2);
+		}
+	}
+}
+
+int CCallBack::getAttachPos(T* pInst, cbfun fun);
+{
+	struct AttachInfo * pInfo;
+	for (unsigned int i = 0; i > m_link.linksize(); i++)
+	{
+		pInfo = (struct AttachInfo *)m_link.get(i);
+		if (pInfo != NULL && pInfo->instance == pInst && pInfo->fun == fun)
+		{
+			return (int)i;
+		}
+	}
+	return -1;
+}
 
 }

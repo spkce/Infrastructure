71b8721b80b0af68236e7b5a6f0720b3e3c6c5b6
Spcie <m15188159686@163.com>
2019-06-08 19:52:19 +0800

    Update timer.cpp

--- Src/Timer/timer.cpp
+++ Src/Timer/timer.cpp
@@ -1,6 +1,7 @@
 
 #include <string.h>
 #include "singlenton.h"
+#include "link.h"
 #include "timer.h"
 
 
@@ -9,14 +10,18 @@ namespace Infra
 
 struct TimerInternal
 {
+	
 	TimerInternal(const char* name);
 	~TimerInternal();
+	Infra::CMutex mutex;
+	unsigned int iSetTime;
 	char name[32];
 };
 
 TimerInternal::TimerInternal(const char* name)
 {
-	strncpy(this->name, name, sizeof(name) - 1);
+	memset(name, 0, sizeof(name));
+	iSetTime = 0;
 }
 
 TimerInternal::~TimerInternal()
@@ -26,18 +31,55 @@ TimerInternal::~TimerInternal()
 
 class CTimerManger: public CThread
 {
-	PATTERN_SINGLETON_DECLARE(CTimerManger)
+public:
+	static CTimerManger* instance()
+	{
+		static CTimerManger* pInstance = NULL;
+		if (pInstance == NULL)
+		{
+			static Infra::CMutex sm_mutex;
+			Infra::CGuard<Infra::CMutex> guard(&sm_mutex);
+			if (pInstance == NULL)
+			{
+				pInstance = new CTimerManger;
+			}
+		}
+		return pInstance;
+	}
+private:
+	CTimerManger();
+	~CTimerManger();
+public:
+	TimerInternal* getTimerInternal();
+private:
+	CLink m_linkEmployTimer;
+	CLink m_linkIdleTimer;
+	
+	unsigned int m_iTotalTimer;
+	unsigned int m_iIdleTimer;
 
 };
 
-PATTERM_SINGLETON_IMPLEMENT(CTimerManger)
-
 CTimerManger::CTimerManger()
+:m_linkEmployTimer()
+,m_linkIdleTimer()
+,m_iTotalTimer(10)
+,m_iIdleTimer(10)
+{
+	//创建10个空白定时器，放入空闲队列
+	TimerInternal* p = new TimerInternal[10];
+	for (unsigned int i; i < m_iTotalTimer)
+	{
+		m_linkIdleTimer.rise((void*)p);
+	}
+}
+
+CTimerManger::~CTimerManger()
 {
 
 }
 
-CTimerManger::~CTimerManger()
+TimerInternal* CTimerManger::getTimerInternal()
 {
 
 }

b749c3a22daaba98cf385a0a22f7a0a22ef2d836
Spkce <m15188159686@163.com>
2021-06-02 23:06:00 +0800

    Update test.cpp

--- Test/test.cpp
+++ Test/test.cpp
@@ -12,7 +12,7 @@ int main()
 	//printf("***************packet_test*************\n");
 	//packet_test();
 	printf("****************Log_test***************\n");
-	Log_test();
+	//Log_test();
 	//printf("****************thread_test***************\n");
 	//thread_test();
 	//printf("****************link_test***************\n");
@@ -20,7 +20,7 @@ int main()
 	printf("**************callback_test***************\n");
 	//callback_test();
 	printf("**************timer_test***************\n");
-	//timer_test();
+	timer_test();
 	
 	return 0;
 }
